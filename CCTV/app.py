ad95de6f42913271aa4d58f1b2516410bdf5bcd0
오희주 <ohj501891@gmail.com>
2025-10-25 09:33:16 +0900

    live cam

--- CCTV/app.py
+++ CCTV/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            frame = cv2.flip(frame, 1)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # flask 스트리밍 응답
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
